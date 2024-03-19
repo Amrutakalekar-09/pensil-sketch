647629426d96eba54f6c10021bc72389b392a7f1
Amrutakalekar-09 <162716699+Amrutakalekar-09@users.noreply.github.com>
2024-03-19 12:18:27 +0530

    Add files via upload

---
+++ photo to pencile sketch project.py
@@ -0,0 +1,15 @@
+import cv2
+img_location ='C:/Users/amrut/.ipython/'
+filename = 'Virat-Kohli-2122.png'
+img = cv2.imread(img_location+filename)
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+inverted_gray_image = 255 - gray_image
+blurred_img = cv2.GaussianBlur(inverted_gray_image, (21,21),0)
+inverted_blurred_img = 255 - blurred_img
+pencil_sketch_IMG = cv2.divide(gray_image, inverted_blurred_img, scale = 256.0)
+#Show the original image
+cv2.imshow('Original Image', img)
+#Show the new image pencil sketch
+cv2.imshow('Pencil Sketch', pencil_sketch_IMG)
+#Display the window infinitely until any keypress
+cv2.waitKey(0)
